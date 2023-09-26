c06caa451e2eb25a065e09cef15a321cef912b32
Paulo Salvatore <salvatoregames@gmail.com>
2023-09-26 00:02:55 +0000

    Conteúdo HTML e CSS movido para componente `Card`

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,15 @@
 import "./Card.css"
 
 export default function Card() {
-    return <div>Card</div>
+  return <div className="card">
+    <h2>Rick Sanchez</h2>
+
+    <div className="tags">
+      <div className="tag">Status: Vivo</div>
+      <div className="tag">Espécie: Humana</div>
+      <div className="tag">Origem: Terra C-137</div>
+    </div>
+
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+  </div>
 }
