fd3a240ca95019b3d759ae60ae5b120ad177fa06
Paulo Salvatore <salvatoregames@gmail.com>
2023-09-26 00:56:52 +0000

    Criação do card 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,16 @@ function App() {
     imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
 
+  // TODO: Criar um segundo item e exibir um card para ele
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
     <>
       <Card item={item1} />
+      <Card item={item2} />
     </>
   )
 }
