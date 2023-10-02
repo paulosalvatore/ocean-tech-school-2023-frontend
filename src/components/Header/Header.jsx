22c8589f78b9adaa4f1fc9be539968589f229bb8
Paulo Salvatore <salvatoregames@gmail.com>
2023-10-02 23:59:27 +0000

    Implementação do `Header`

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
 import "./Header.css";
 
 export default function Header() {
-  return <header className="header">Header está funcionando!</header>;
+  return (
+    <header className="header">
+      <a href="/" className="logo-link">
+        <img
+          src="https://oceanbrasil.com/assets/logo.svg"
+          alt="Logo do Ocean"
+        />
+      </a>
+
+      <nav>
+        <a href="/">Home</a>
+
+        <a href="/criar-personagem">Criar personagem</a>
+      </nav>
+    </header>
+  );
 }
